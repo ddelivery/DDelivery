a618694b68ae25b65f03c697619a617dae2ae7e5
DnAp <dnlebedev@gmail.com>
2014-04-22 15:35:54 +0400

    Фикс курьерки

--- public_html/html/js/ddelivery.courier.js
+++ public_html/html/js/ddelivery.courier.js
@@ -20,6 +20,26 @@ var Courier = (function(){
             $('.map-popup__main__delivery').mCustomScrollbar({
                 scrollInertia: 0
             });
+            $('.map-popup__main__delivery input[type="radio"]').Custom({
+                customStyleClass: 'radio',
+                customHeight: '20'
+            });
+
+            var mapPopupTableTr = $('.map-popup__main__delivery table tr');
+            mapPopupTableTr.hover(function () {
+                if(!$(this).hasClass('disabled')){
+                    $(this).addClass('hover');
+                }
+            }, function () {
+                $(this).removeClass('hover');
+            });
+            mapPopupTableTr.on('click', function (e) {
+                e.preventDefault();
+                if(!$(this).hasClass('disabled')){
+                    var radio = $(this).find('input[type="radio"]');
+                    radio.prop('checked', true).change();
+                }
+            });
 
         }
     }
