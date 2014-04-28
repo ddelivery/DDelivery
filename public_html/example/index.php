3f45906aad8b0bb997be7111ddc2e9483ea8d7dc
DnAp <dnlebedev@gmail.com>
2014-04-28 11:04:24 +0400

    Чуть более внятные описания в примерах

--- public_html/example/index.php
+++ public_html/example/index.php
@@ -6,10 +6,21 @@
     <body>
         <script src="../html/js/ddelivery.js"></script>
         <div id="ddelivery"></div>
-        <div style="width: 100px; height: 300px"></div>
+        <a href="javascript:DDeliveryStart()">Выбрать способ доставки</a>
 
         <script>
-            DDelivery.delivery('ddelivery', 'ajax.php<?isset($_GET['XDEBUG_SESSION_START'])?'XDEBUG_SESSION_START='.(int)$_GET['XDEBUG_SESSION_START']:''?>', {});
+            function DDeliveryStart(){
+                var data = {
+                    //orderId: 4, // Если у вас есть id заказа который изменяется, то укажите его в этом параметре
+                    close: function(){
+                        alert('Окно закрыто');
+                    },
+                    change: function(orderId, aboutDdelivery, amount) {
+                        alert('Выбрали '+aboutDdelivery+ ' интернет магазину нужно взять с пользователя '+amount+' руб.');
+                    }
+                };
+                DDelivery.delivery('ddelivery', 'ajax.php', data);
+            }
         </script>
     </body>
 </html>
