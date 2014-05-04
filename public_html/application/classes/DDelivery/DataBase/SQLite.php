ca40ae8263af78e633c4bff28786a68d31d6d47d
mrozk <mrozk2012@gmail.com>
2014-05-04 21:33:11 +0300

    Добавление проверки прав доступа к папке

--- public_html/application/classes/DDelivery/DataBase/SQLite.php
+++ public_html/application/classes/DDelivery/DataBase/SQLite.php
@@ -23,6 +23,14 @@ class SQLite {
         if(!self::$dbUri)
             throw new DDeliveryException('SQLite::dbUri is empty');
         if ( empty(self::$pdo) ) {
+            // Проверка прав доступа к папке
+            $dirs = explode(DIRECTORY_SEPARATOR, self::$dbUri );
+            array_pop($dirs);
+            $dbPath = implode( DIRECTORY_SEPARATOR, $dirs);
+            if(  (!is_writable( $dbPath )) || ( !is_writable( self::$dbUri ) ) || (!is_dir( $dbPath )) )
+            {
+                throw new DDeliveryException('Папка с БД SQLite не существует или недоступна для записи');
+            }
             self::$pdo = new \PDO('sqlite:'.self::$dbUri);
             self::$pdo->exec('PRAGMA journal_mode=WAL;');
         }
