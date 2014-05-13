c78597229747f21249e6b308773d4e9b980a0774
DnAp <dnlebedev@gmail.com>
2014-05-13 14:23:03 +0400

    Метод проверки на валидность ключа

--- public_html/application/classes/DDelivery/Sdk/DDeliverySDK.php
+++ public_html/application/classes/DDelivery/Sdk/DDeliverySDK.php
@@ -377,6 +377,15 @@ class DDeliverySDK {
         return $response;
     }
 
+    /**
+     * Возвращает true если ключ валиден
+     * @return bool
+     */
+    function checkApiKey()
+    {
+        $result = $this->requestProvider->request('order_status');
+        return $result->errorMessage != 'Shop not found!';
+    }
 
     /**
      * Возвращает id городов с болшим кол-вом людей, может когда-нибудь будет на сервере
