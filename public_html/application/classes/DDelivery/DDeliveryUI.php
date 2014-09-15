2ecaebbc973da647bb0698c55c8791bebe583100
mrozk <mrozk2012@gmail.com>
2014-09-15 18:11:14 +0300

    Исправлен баг интервалов цены

--- public_html/application/classes/DDelivery/DDeliveryUI.php
+++ public_html/application/classes/DDelivery/DDeliveryUI.php
@@ -866,7 +866,7 @@ use DDelivery\Sdk\Messager;
                 $price = $companyArray['delivery_price'];
             }
             // интервалы
-            $this->shop->preDisplayPointCalc($price);
+            $price = $this->shop->preDisplayPointCalc($price);
             // Ручное редактирование
             $price = $this->shop->processClientPrice( $order, $price );
 
