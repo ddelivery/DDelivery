03af19554203bbcb321dd9f12b9b272867c26941
mrozk <mrozk2012@gmail.com>
2014-10-03 10:34:52 +0300

    testing xprof

--- public_html/example/ajax.php
+++ public_html/example/ajax.php
@@ -8,7 +8,7 @@ include_once(implode(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'application', 'b
 include_once("IntegratorShop.php");
 
 use DDelivery\DDeliveryUI;
-xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+// xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 try{
     $IntegratorShop = new IntegratorShop();
     $ddeliveryUI = new DDeliveryUI($IntegratorShop);
@@ -16,11 +16,14 @@ try{
     $ddeliveryUI->render(isset($_REQUEST) ? $_REQUEST : array());
 }catch ( \DDelivery\DDeliveryException $e ){
     echo $e->getMessage();
+
     $IntegratorShop->logMessage($e);
 }
+/*
 $xhprof_data = xhprof_disable();
 include_once "/var/www/html/xhprof-0.9.4/xhprof_lib/utils/xhprof_lib.php";
 include_once "/var/www/html/xhprof-0.9.4/xhprof_lib/utils/xhprof_runs.php";
 $xhprof_runs = new XHProfRuns_Default();
 $run_id = $xhprof_runs->save_run($xhprof_data, "test");
+*/
 
