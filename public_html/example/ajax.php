c949b583102448509886960eba0bfb2f95ee485b
mrozk <mrozk2012@gmail.com>
2014-10-29 14:06:36 +0200

    ТЕст

--- public_html/example/ajax.php
+++ public_html/example/ajax.php
@@ -10,6 +10,7 @@ include_once("IntegratorShop.php");
 use DDelivery\DDeliveryUI;
 // xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 try{
+    echo 'xxxx';
     $IntegratorShop = new IntegratorShop();
     $ddeliveryUI = new DDeliveryUI($IntegratorShop);
     // В зависимости от параметров может выводить полноценный html или json
